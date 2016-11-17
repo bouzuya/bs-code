7803a0305cfb4c0440fdae211e75e9dfa11f0251
bouzuya <m@bouzuya.net>
2016-11-18 05:32:52 +0900

    extract activeFilePath()

--- src/commands/open-pair-file.ts
+++ src/commands/open-pair-file.ts
@@ -34,12 +34,20 @@ const nextPairFilePath = (filePath: string): string | null => {
   return nextFilePath;
 };
 
-const openPairFile = () => {
-  const editor: TextEditor | undefined = window.activeTextEditor;
-  if (typeof editor === 'undefined') return; // No open text editor
+const activeFilePath = (
+  window: { activeTextEditor: TextEditor | undefined }
+): string | null => {
+  const editor = window.activeTextEditor;
+  if (typeof editor === 'undefined') return null; // No active text editor
   const document = editor.document;
-  if (document.isUntitled) return; // No file
+  if (document.isUntitled) return null; // No file
   const filePath = document.fileName;
+  return filePath;
+};
+
+const openPairFile = (): void => {
+  const filePath = activeFilePath(window);
+  if (filePath === null) return;
   const pairFilePath = nextPairFilePath(filePath);
   if (pairFilePath === null) return; // No next pair file
   workspace.openTextDocument(pairFilePath).then((document) => {
