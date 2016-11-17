0dfd8c8ad407d1938e6f7f49388c09212c432528
bouzuya <m@bouzuya.net>
2016-11-18 06:11:53 +0900

    add test

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,16 +1,31 @@
 import * as assert from 'assert';
+import * as proxyquire from 'proxyquire';
+import * as sinon from 'sinon';
+import { activate } from '../src/extension';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
-import * as myExtension from '../src/extension';
-
-// Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", () => {
-
-    // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+suite('Extension Tests', () => {
+  test('bsCode.insertMarkdownAnchors', () => {
+    const push = sinon.stub();
+    const context = { subscriptions: { push } };
+    const registerCommand = sinon.stub();
+    registerCommand.onCall(0).returns('command1');
+    registerCommand.onCall(1).returns('command2');
+    const commands = { registerCommand };
+    proxyquire('../src/extension', {
+      'vscode': { commands }
     });
+    activate(<any>context);
+    assert(context.subscriptions.push.callCount === 2);
+    const [command1] = commands.registerCommand.getCall(0).args;
+    assert(command1 === 'command1');
+    const [command2] = commands.registerCommand.getCall(1).args;
+    assert(command2 === 'command2');
+    assert(commands.registerCommand.callCount === 2);
+    const [name1, fn1] = commands.registerCommand.getCall(0).args;
+    assert(name1 === 'bsCode.insertMarkdownAnchors');
+    assert(fn1);
+    const [name2, fn2] = commands.registerCommand.getCall(1).args;
+    assert(name2 === 'bsCode.insertMarkdownAnchors');
+    assert(fn2);
+  });
 });
