373dbedfd852fb7aea6837657b44302abea8f1b1
bouzuya <m@bouzuya.net>
2016-11-19 08:54:15 +0900

    refactor

--- src/extension.ts
+++ src/extension.ts
@@ -2,13 +2,16 @@ import { ExtensionContext, commands } from 'vscode';
 import { insertMarkdownAnchors } from './commands/insert-markdown-anchors';
 import { openPairFile } from './commands/open-pair-file';
 
+const bsCodeCommands = [
+  { name: 'bsCode.insertMarkdownAnchors', fn: insertMarkdownAnchors },
+  { name: 'bsCode.openPairFile', fn: openPairFile }
+];
+
 export function activate(context: ExtensionContext): void {
-  context.subscriptions.push(commands.registerCommand(
-    'bsCode.insertMarkdownAnchors',
-    insertMarkdownAnchors));
-  context.subscriptions.push(commands.registerCommand(
-    'bsCode.openPairFile',
-    openPairFile));
+  bsCodeCommands.forEach(({ name, fn }) => {
+    const disposable = commands.registerCommand(name, fn);
+    context.subscriptions.push(disposable);
+  });
 }
 
 export function deactivate(): void {
