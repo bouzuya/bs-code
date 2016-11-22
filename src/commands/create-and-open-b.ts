1eeff4a097f61bb3f8929c61d086182b20207acb
bouzuya <m@bouzuya.net>
2016-11-23 07:23:50 +0900

    add bsCode.rootDirectory config

--- src/commands/create-and-open-b.ts
+++ src/commands/create-and-open-b.ts
@@ -1,3 +1,29 @@
-const createAndOpenB = (): void => void 0; // TODO
+import * as fs from 'fs';
+import { TextEditor, window, workspace } from 'vscode';
+
+const getRootDirectory = (): string | null => {
+  const config = workspace.getConfiguration('bsCode');
+  const rootDirectory: string | null = config.get('rootDirectory', null);
+  return rootDirectory;
+};
+
+const getRootDirectoryError = (rootDirectory: string | null): string => {
+  if (rootDirectory === null) {
+    return 'bsCode.rootDirectory is null';
+  }
+  if (!fs.statSync(rootDirectory).isDirectory) {
+    return 'bsCode.rootDirectory is not directory';
+  }
+  return null;
+};
+
+const createAndOpenB = (): void => {
+  const rootDirectory = getRootDirectory();
+  const rootDirectoryError = getRootDirectoryError(rootDirectory);
+  if (rootDirectoryError !== null) {
+    window.showErrorMessage(rootDirectoryError);
+    return;
+  }
+};
 
 export { createAndOpenB };
