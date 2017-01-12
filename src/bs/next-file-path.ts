7c5e109698ddf40dbabb1c9b38db530cafe63daa
bouzuya <m@bouzuya.net>
2017-01-12 09:20:29 +0900

    add nextFilePath()

---
+++ src/bs/next-file-path.ts
@@ -0,0 +1,18 @@
+import { join } from 'path';
+import { getExtension } from './get-extension';
+import { getFilePaths } from './get-file-paths';
+
+const nextFilePath = (
+  rootDirectory: string,
+  currentFilePath: string
+): string | null => {
+  const extName = getExtension(currentFilePath);
+  const flowDirectory = join(rootDirectory, 'flow');
+  const filePaths = getFilePaths(flowDirectory).filter((i) => getExtension(i) === extName);
+  const index = filePaths.indexOf(currentFilePath);
+  if (index < 0) return null; // current file is not found
+  if (index + 1 >= filePaths.length) return null; // next file is not found
+  return filePaths[index + 1];
+};
+
+export { nextFilePath };
