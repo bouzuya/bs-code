3f862c32212adc876d6800c0f5f4c2359629d423
bouzuya <m@bouzuya.net>
2017-07-29 16:37:04 +0900

    `open selected file` match ambiguous date

--- src/commands/open-selected-file.ts
+++ src/commands/open-selected-file.ts
@@ -28,13 +28,17 @@ const openSelectedFile = (): void => {
   }
   const selectedText = getSelectedText();
   if (selectedText === null) return;
-  const match = selectedText.trim().match(/^(\d{4})(\d{2})(\d{2})T\d{6}Z$/);
+  const text = new Date(selectedText.trim())
+    .toISOString()
+    .replace(/\.\d{3}Z$/, 'Z')
+    .replace(/[-:+]/g, '');
+  const match = text.match(/^(\d{4})(\d{2})(\d{2})T\d{6}Z$/);
   if (match === null) return;
   const year = match[1];
   const month = match[2];
   const date = match[3];
   const path = join(
-    rootDirectory, 'flow', year, month, date, selectedText + '.md'
+    rootDirectory, 'flow', year, month, date, text + '.md'
   );
   const paths = pairFilePaths(path);
   workspace.openTextDocument(paths[1]).then((document) => {
