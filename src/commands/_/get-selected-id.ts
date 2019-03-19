1f943cf87c7380b88a5e4c57f2e03a7d9fcec96e
bouzuya <m@bouzuya.net>
2019-03-19 13:09:31 +0900

    fix getSElectedId

--- src/commands/_/get-selected-id.ts
+++ src/commands/_/get-selected-id.ts
@@ -1,4 +1,3 @@
-import { window } from 'vscode';
 import { getCursorText } from '../_/get-cursor-text';
 import { getSelectedText } from '../_/get-selected-text';
 import { BID, fromString } from '../../bs/bid';
@@ -8,7 +7,9 @@ const getSelectedId = (): BID | null => {
     return s1 !== null ? s1 : s2;
   };
   const selectedText = alt(getSelectedText(), getCursorText(/[0-9TZ+:-]+/));
-  if (selectedText === null) return;
+  if (selectedText === null) return null;
+  const idPrefix = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/;
+  if (selectedText.trim().match(idPrefix) === null) return null;
   const text = new Date(selectedText.trim())
     .toISOString()
     .replace(/\.\d{3}Z$/, 'Z')
